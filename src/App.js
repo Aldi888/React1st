7c1e627919d48a800bb3476d5b4078d10391277c
Aldi888 <aldiberg@gmail.com>
2021-11-04 19:37:26 -0300

    divclass

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
       <div>
         <h1>Modal Template ToDo</h1>
       </div>
+      <div className='card'>
       <h2>TITLE</h2>
       <div>
         <button>Delete</button>
+      </div>
       </div>
     </div>
   );
