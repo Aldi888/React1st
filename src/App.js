88c5da665f2ae5411eef3dfab44a8c79f403ed37
Aldi888 <aldiberg@gmail.com>
2021-11-04 20:08:11 -0300

    5353

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ToDo from "./components/Todo";
 import logo from "./logo.svg";
 
 function App() {
@@ -6,12 +7,9 @@ function App() {
       <div>
         <h1>Modal Template ToDo</h1>
       </div>
-      <div className='card'>
-      <h2>TITLE</h2>
-      <div className='actions'>
-        <button className='btn'>Delete</button>
-      </div>
-      </div>
+      <ToDo />
+      <ToDo />
+      <ToDo />
     </div>
   );
 }
