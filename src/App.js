ddf8f14df64630ab4686790995ac78e81bc64b6e
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-09 17:56:10 -0300

    colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import { useState, useEffect, FormEvent } from 'react';
+import React from 'react';
 //import { StyledComponentClass } from 'styled-components';
 import ToDo from "./components/Todo";
 //import Modal from "./components/Modal";
@@ -6,23 +6,24 @@ import ToDo from "./components/Todo";
 //import Backdrop from "./components/Backdrop";
 import * as C from "./components/App.styles";
 
+
 function App() {
   return (
     <C.Container>
-    <div>
       <div>
-        <h1>Modal Template</h1>
+        <div>
+          <C.Header><h1>Modal Template</h1></C.Header>
+        </div>
+        <C.Blocklist>
+          <ToDo text="MADE WITH" />
+          <ToDo text="REACTJS" />
+          <ToDo text="PORTFOLIO" />
+          <ToDo text="DEVOPS" />
+        </C.Blocklist>
       </div>
-      <C.Blocklist>
-      <ToDo text='FEITO NA UNHA'/>
-      <ToDo text='COM REACTJS'/>
-      <ToDo text='PORTFOLIO'/>
-      <ToDo text='DEVOPS'/>
-    
-    </C.Blocklist>
-    </div>
     </C.Container>
   );
 }
+
 
 export default App;
--- src/components/App.styles.ts
+++ src/components/App.styles.ts
@@ -5,15 +5,16 @@ export const Header = styled.h1`
     margin: 0;
     padding: 0;
     text-align: center;
-    margim-bottom: 30px;
+    margin-bottom: 30px;
+    color: #ffffff;
 `;
 export const Container = styled.div`
-
-    bbackground-color: rgba(0, 0, 0, 0.75);
-    color: #FFF;
+    background-color: #214184;
+    color: #282c34;
     min-height: 100vh;
     text-align: center;
-    margim-bottom: 30px;
+    margin-bottom: 30px;
+    border-radius: 11px;
 `;
 
 export const Area = styled.div`
@@ -30,8 +31,20 @@ export const Blocklist = styled.div`
 font-size: 15px;
 border-radius: 10px;
 margin: 0 20px;
+background-color: #4374b7;
+
 `;
 
+export const Btn = styled.button `
+font: inherit;
+padding: 0.5rem 1.5rem;
+cursor: pointer;
+border-radius: 4px;
+background-color: #800040;
+color: white;
+border: 1px solid #800040;
+margin: 0 1rem;
 
+`;
 
 export default styled;
