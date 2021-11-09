fa79549fbe8a377a71e9b01d60105d57890de2ac
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-09 18:12:57 -0300

    plus button

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
           <ToDo text="REACTJS" />
           <ToDo text="PORTFOLIO" />
           <ToDo text="DEVOPS" />
+          <ToDo text="PUBLISHED" />
+          <ToDo text="AT AZURE" />
+          <ToDo text="STATIC WEB APP" />
+          <ToDo text="USING GITHUB" />
         </C.Blocklist>
       </div>
     </C.Container>
